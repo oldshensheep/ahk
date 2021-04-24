13969eb9657bc362b027b73361b69bbc900edda0
oldshensheep <29306733+oldshensheep@users.noreply.github.com>
2021-04-24 19:28:22 +0800

    move ahk to src

--- file4make/make.ps1
+++ file4make/make.ps1
@@ -1,4 +1,4 @@
-Get-ChildItem -Path ./* -Include *.ahk | ForEach-Object -Process {
+Get-ChildItem -Path ./src/* -Include *.ahk | ForEach-Object -Process {
     New-Item -Path "out" -ItemType Directory
     $eString = ".\file4make\Compiler\ahk2exe.exe /in $($_.Name)  /out out/$($_.Name).exe"
     Invoke-Expression $eString
