a5e74d9c6cd62fb869a3b06e896ea7a43715f5e1
oldshensheep <29306733+oldshensheep@users.noreply.github.com>
2021-04-24 19:25:27 +0800

    Upload a Build Artifact

--- file4make/make.ps1
+++ file4make/make.ps1
@@ -1,6 +1,6 @@
 Get-ChildItem -Path ./* -Include *.ahk | ForEach-Object -Process {
     New-Item -Path "out" -ItemType Directory
-    $eString = ".\file4make\Compiler\ahk2exe.exe /in $($_.Name)  /out out/$($_.Name).ahk"
+    $eString = ".\file4make\Compiler\ahk2exe.exe /in $($_.Name)  /out out/$($_.Name).exe"
     Invoke-Expression $eString
     Write-Host($eString)
 } 
