e92d8edeec2a276d549a9dc1d794431ff33ad8d1
oldshensheep <29306733+oldshensheep@users.noreply.github.com>
2021-05-08 20:15:50 +0800

    update make

--- file4make/make.ps1
+++ file4make/make.ps1
@@ -1,7 +1,7 @@
 $loc = Get-Location
 New-Item -Path "out" -ItemType Directory
 Get-ChildItem -Path ./src/* -Include *.ahk | ForEach-Object -Process {
-    $eString = ".\file4make\Compiler\ahk2exe.exe /in $($_)  /out $loc/out/$($_.Name).exe"
+    $eString = "./file4make/Compiler/ahk2exe.exe /in $($_)  /out $loc/out/$($_.Name).exe"
     Invoke-Expression $eString
     Write-Host($eString)
-} 
+}
